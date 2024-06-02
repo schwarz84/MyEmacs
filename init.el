ba045ee33906f529a70677b381c8714ab5636a2e
Charly <carlosschwarzfischer@gmail.com>
2024-06-02 12:02:59 -0300

    cambios 02/06/2024

--- init.el
+++ init.el
@@ -1,14 +1,14 @@
-;;; init.el --- Archivo de Configuracion Personal de Emacs -*- lexical-binding: t -*-
+;;; init.el --- Archivo de Configuración Personal de Emacs -*- lexical-binding: t -*-
 
 ;; Autor: Carlos Schwarz
 ;; Mantenimiento: Carlos Schwarz
-;; Version: 1.0
+;; Versión: 1.0
 
 ;; Este archivo no forma parte del proyecto Emacs
 
-;; Este archivo es software libre, podes redistribuirlo y/o modificarlo
-;; bajo los términos de GNU General Public License como esta publicado en
-;; La Free Software Foundation
+;; Este archivo es software libre; puedes redistribuirlo y/o modificarlo
+;; bajo los términos de la GNU General Public License como está publicado en
+;; la Free Software Foundation
 
 ;; Este programa se distribuye con la esperanza de que sea de utilidad,
 ;; pero SIN NINGUNA GARANTÍA; sin siquiera la garantía implícita de
@@ -16,89 +16,89 @@
 ;; GNU General Public License para más detalles.
 
 ;; Para una copia completa de la GNU General Public License
-;; ir a <http://www.gnu.org/licenses/>.
-
+;; ve a <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; Esta es mi Setup creada después de años de pruebas y dedicación.
-;; No todo el credito es mio sino a la increible comunidad de Emacs que existe.
+;; Esta es mi configuración creada después de años de pruebas y dedicación.
+;; No todo el crédito es mío, sino también de la increíble comunidad de Emacs.
 
 ;;; Code:
 
-;; Por defecto se establece en 800 kilobytes. Medida en Bytes
+;; Ajustar el límite del recolector de basura a 50MB para mejorar el rendimiento inicial.
 (setq gc-cons-threshold (* 50 1000 1000))
 
-;; Calculo de tiempo de arranque don datos de la basura
+;; Calcular el tiempo de arranque con detalles sobre el recolector de basura.
 (add-hook 'emacs-startup-hook
           (lambda ()
-          ;; Restablecer gc-cons-threshold después del inicio
-            (setq gc-cons-threshold (* 800 1024))
-            (message "*** Emacs se cargo en %s con %d basura."
-                     (format "%.2f seconds"
+            ;; Restablecer gc-cons-threshold después del inicio.
+            (setq gc-cons-threshold (* 8 1000 1000))
+            (message "*** Emacs se cargó en %s con %d recolecciones de basura."
+                     (format "%.2f segundos"
                              (float-time
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
 
-;; Que el compilado de errores trabaja en segundo plano
+;; Hacer que los errores de compilación trabajen en segundo plano.
 (setq comp-async-report-warnings-errors nil)
 
-;; Incio las fuentes de paquetes
+;; Inicializar los paquetes.
 (require 'package)
-
 (setq package-archives '(
-    ("melpa" . "https://melpa.org/packages/")
-    ("melpa-stable" . "https://stable.melpa.org/packages/")
-    ("org" . "https://orgmode.org/elpa/")
-    ("elpa" . "https://elpa.gnu.org/packages/")
-  )
-)
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")
+                         ))
 
-;; Condicion si existe el paquete ya esta instalado actualizarlo
 (package-initialize)
-
 (unless package-archive-contents
-(package-refresh-contents))
+  (package-refresh-contents))
 
-;; Configuracion si se instala en un SO no Linux
+;; Configurar e instalar `use-package` si no está ya instalado.
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
 ;; Bootstrap straight.el
 (defvar bootstrap-version)
-(let (
-  (bootstrap-file
-    (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-    (bootstrap-version 5))
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-      (url-retrieve-synchronously
-      "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-      'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; Siempre usar straight para instalar en sistemas no linux
+;; Siempre usar straight para instalar paquetes, excepto en GNU/Linux.
 (setq straight-use-package-by-default (not (eq system-type 'gnu/linux)))
 
-;; Usar straight.el para expresiones de use-package
+;; Usar straight.el con use-package.
 (straight-use-package 'use-package)
-;; (setq straight-use-package-by-default t)
 
-;; Cargar el helper package para estos comandos `straight-x-clean-unused-repos'
+;; Cargar el helper package `straight-x`.
 (require 'straight-x)
 
-;; Perzonalizacion de los archivos temporales
+;; Personalización de los archivos temporales.
 (setq custom-file
-  (if (boundp 'server-socket-dir)
-    (expand-file-name "custom.el" server-socket-dir)
-    (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
+      (if (boundp 'server-socket-dir)
+          (expand-file-name "custom.el" server-socket-dir)
+        (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
 (load custom-file t)
 
-;; Carga de org files
-(org-babel-load-file (expand-file-name "~/.emacs.d/config/packages.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config/ui_theme.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config/keys_and_functions.org"))
-(org-babel-load-file (expand-file-name "~/.emacs.d/config/languajes.org"))
+;; Cargar archivos org, verificando su existencia.
+(dolist (file '("~/.emacs.d/config/packages.org"
+                "~/.emacs.d/config/ui_theme.org"
+                "~/.emacs.d/config/keys_and_functions.org"
+                "~/.emacs.d/config/languages.org"))
+  (when (file-exists-p (expand-file-name file))
+    (org-babel-load-file (expand-file-name file))))
+
+;; Habilitar la función `narrow-to-region`.
 (put 'narrow-to-region 'disabled nil)
+
+(provide 'init)
+;;; init.el ends here
